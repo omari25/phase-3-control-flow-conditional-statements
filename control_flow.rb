b2dfcfef224e34329ffdd72562a3346d37744395
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-08-15 19:48:58 +0300

    Done.

--- control_flow.rb
+++ control_flow.rb
@@ -7,15 +7,7 @@ def admin_login(username, password)
 end
 
 def hows_the_weather(temperature)
-  if temperature < 50
-    "It's brisk out there!"
-  elsif temperature >= 40 && temperature <= 65
-    "It's a little chilly out there!"
-  elsif temperature > 85
-    "It's too dang hot out there!"
-  else
-    "It's perfect out there!" 
-  end
+  # your code here
 end
 
 def fizzbuzz(num)
